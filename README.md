775bfd79e58c1f43d0720e83f7e2f296eb819509
Nursel Cansu Özkan <nurselcansuozkan@gmail.com>
2022-06-08 00:56:48 +0300

    koşullu render işlemi

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # reactjs
+# create-react-app ile react projesini ayağa kaldırma
+# Component oluşturma, kullanma
+# JSX kuralları
+# Componentlerde değişken render etmek
+# Koşullu render işlemi
--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -3,6 +3,11 @@ import './App.css';
 import Header from "./components/Header";
 import React from 'react';
 
+
+const name = "Nursel";
+const surname = "Ozkan";
+const isLoggedIn = false;
+
 function App() {
   // //Bu şekilde de kullanılabilir ama çok fazla bileşen varken mantıklı değil
   // return React.createElement('div', null, 'Hello');
@@ -11,21 +16,14 @@ function App() {
   return (
     //Kapsayıcı element yoksa syntax hatası verir <></>, <React.Fragment> da kullanılabilir
     <div>
-      <h1>Hello React</h1>
-      <Header />
-      <p className="xyz">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit, 
-      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-      nisi ut aliquip ex ea commodo consequat. 
-      </p>
-
-      <label htmlFor="myInput">
-      Name
-      <input type="text" id='myInput' />
-      </label>
-
-
+      <h1>{name}</h1>
+      //Koşullu render işlemi
+      <h1>
+        { isLoggedIn
+          ? `Benim adım ${name}, soyadım ${surname}`
+          : "Giriş yapmadınız"
+        }
+      </h1>
     </div>
   );
 }
